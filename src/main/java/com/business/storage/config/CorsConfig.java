c6dff880b5b8107f135a6b2ceec0f30e7cbdd9c0
gf63 <makson4747@mail.ru>
2020-05-16 13:21:38 +0300

    add cors config

---
+++ src/main/java/com/business/storage/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.business.storage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: mmustafin@context-it.ru
+ * @created: 15.05.2020
+ */
+@Configuration
+public class CorsConfig {
+
+    private static final String FRONTEND_URL = "http://localhost:3000";
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/question/*").allowedOrigins(FRONTEND_URL);
+            }
+        };
+    }
+
+}
